0bd8541c042abf2bc259c91557c23c7acef63882
Reed0ne <danyelgianmarquesdasilva@gmail.com>
2023-11-29 17:32:17 -0300

    Saving a new Department

--- src/gui/util/Utils.java
+++ src/gui/util/Utils.java
@@ -6,6 +6,13 @@ import javafx.stage.Stage;
 
 public class Utils {
 
+	public static Integer tryParseToInt(String str) {
+		try {
+			return Integer.parseInt(str);
+		} catch (NumberFormatException e) {
+			return null;
+		}
+	}
 	
 	public static Stage currentStage(ActionEvent event) {
 		return (Stage) ((Node) event.getSource()).getScene().getWindow();
